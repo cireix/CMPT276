9b956a743dfbaffe61eaed52db8b121940b6171d
Eric Xie <e@Erics-MacBook-Pro.local>
2020-07-02 19:58:49 -0700

    Initial Server Commit- Registration, Login, Validation

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI: "mongodb://admin:a7as2lifBeh8pr0B@alcoholdelivery-shard-00-00-oqe6g.mongodb.net:27017,alcoholdelivery-shard-00-01-oqe6g.mongodb.net:27017,alcoholdelivery-shard-00-02-oqe6g.mongodb.net:27017/alcohol?ssl=true&replicaSet=alcoholDelivery-shard-0&authSource=admin&retryWrites=true&w=majority",
+	secretOrKey: "secret"
+};
