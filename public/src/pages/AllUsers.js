3158256102a34e1a6ab0a72747a7b29e05c9dea2
Shiyuan Miao <shiyuanm@sfu.ca>
2020-07-08 22:46:30 -0700

    add header to every page

--- public/src/pages/AllUsers.js
+++ public/src/pages/AllUsers.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+import Header from 'components/Header';
 
 class AllUsers extends React.Component {
+    
     headers = ['User Id', 'Name', 'Phone Number', 'Type'];
     state = {
         users : []
@@ -15,29 +17,33 @@ class AllUsers extends React.Component {
     }
 
     render() {
+        const user = global.auth.getUser();
         return (
-            <div className="allusers">
-                <table className="table is-fullwidth">
-                    <thead>
-                        <tr>
-                            {this.headers.map(head => <th>{head}</th>)}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.users.map(row =>
-                                <tr>
-                                    <td key={row._id}>{row._id}</td>
-                                    <td key={row.name}>{row.name}</td>
-                                    <td key={row.phone}>{row.phone}</td>
-                                    <td key={row.type}>{row.type === 1 ?"Admin" : [row.type === 2 ? "Driver":"General User"]}</td>
-                                </tr>
-                            
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
+            <React.Fragment>
+                <Header user={user} />
+                <div className="allusers">
+                    <table className="table is-fullwidth">
+                        <thead>
+                            <tr>
+                                {this.headers.map(head => <th>{head}</th>)}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.users.map(row =>
+                                    <tr>
+                                        <td key={row._id}>{row._id}</td>
+                                        <td key={row.name}>{row.name}</td>
+                                        <td key={row.phone}>{row.phone}</td>
+                                        <td key={row.type}>{row.type === 1 ?"Admin" : [row.type === 2 ? "Driver":"General User"]}</td>
+                                    </tr>
+                                
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </React.Fragment>
         )
     }
 }
--- public/src/pages/ForgotPw.js
+++ public/src/pages/ForgotPw.js
@@ -2,8 +2,11 @@ import React, {useState} from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import Header from 'components/Header';
 
 export default function ForgotPw(props) {
+    const user = global.auth.getUser();
+
     const { register, handleSubmit, errors, watch } = useForm();
     var [codeSent,setCodeSent] = useState(false);
     // Get phone number that user input in the form
@@ -39,102 +42,105 @@ export default function ForgotPw(props) {
     }
 
     return (
-        <div className="login_wrapper">
-            <form className="box login_box" >
+        <React.Fragment>
+            <Header user={user} />
+            <div className="login_wrapper">
+                <form className="box login_box" >
 
-                <div className="field">
-                    <label className="label">Phone Number</label>
-                    <div class="field-body">
-                        <div class="field is-expanded">
-                            <div class="field has-addons">
-                                <p class="button is-static">+1</p>
-                                <input
-                                    type="text"
-                                    className={`input ${errors.phoneNumber && 'is-danger'}`}
-                                    placeholder="Phone number"
-                                    name="phoneNumber"
-                                    ref={register({
+                    <div className="field">
+                        <label className="label">Phone Number</label>
+                        <div class="field-body">
+                            <div class="field is-expanded">
+                                <div class="field has-addons">
+                                    <p class="button is-static">+1</p>
+                                    <input
+                                        type="text"
+                                        className={`input ${errors.phoneNumber && 'is-danger'}`}
+                                        placeholder="Phone number"
+                                        name="phoneNumber"
+                                        ref={register({
 
-                                        pattern: {
-                                            value: /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/,
-                                            message: "invalid phone number"
-                                        }
-                                    })} />
+                                            pattern: {
+                                                value: /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/,
+                                                message: "invalid phone number"
+                                            }
+                                        })} />
+                                </div>
+                                {errors.phoneNumber && <p className="helper has-text-danger">{errors.phoneNumber.message}</p >}
                             </div>
-                            {errors.phoneNumber && <p className="helper has-text-danger">{errors.phoneNumber.message}</p >}
                         </div>
                     </div>
-                </div>
 
-                <div className="control">
-                    <button className="button is-link login_button" onClick={handleSubmit(onSubmit)}>Get Authentication Code</button>
-                </div>
-
-                <br />
-
-                <div className="field">
-                    <label className="label">Authentication Code</label>
                     <div className="control">
-                        <input
-                            disabled = {(codeSent)?"":"disabled"}
-                            className={`input ${errors.code && 'is-danger'}`}
-                            type="number"
-                            placeholder="Authentication Code"
-                            name='code'
-                            ref={register({
-                                maxLength: {
-                                    value: 6,
-                                    message: 'should be 6 digits'
-                                }
-                            })} />
-                        {errors.code && <p className="helper has-text-danger">{errors.code.message}</p>}
+                        <button className="button is-link login_button" onClick={handleSubmit(onSubmit)}>Get Authentication Code</button>
                     </div>
-                </div>
 
-                <div className="field">
-                    <label className="label">Password</label>
+                    <br />
+
+                    <div className="field">
+                        <label className="label">Authentication Code</label>
+                        <div className="control">
+                            <input
+                                disabled = {(codeSent)?"":"disabled"}
+                                className={`input ${errors.code && 'is-danger'}`}
+                                type="number"
+                                placeholder="Authentication Code"
+                                name='code'
+                                ref={register({
+                                    maxLength: {
+                                        value: 6,
+                                        message: 'should be 6 digits'
+                                    }
+                                })} />
+                            {errors.code && <p className="helper has-text-danger">{errors.code.message}</p>}
+                        </div>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Password</label>
+                        <div className="control">
+                            <input
+                                disabled = {(codeSent)?"":"disabled"}
+                                className={`input ${errors.password && 'is-danger'}`}
+                                type="password"
+                                placeholder="Password"
+                                name='password'
+                                ref={register({
+                                    //required: true,
+                                    minLength: {
+                                        value: 6,
+                                        message: 'cannot be less than 6 digits'
+                                    }
+                                })} />
+                            {errors.password && <p className="helper has-text-danger">{errors.password.message}</p>}
+                        </div>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Confirm Password</label>
+                        <div className="control">
+                            <input
+                                disabled = {(codeSent)?"":"disabled"}
+                                className={`input ${errors.password2 && 'is-danger'}`}
+                                type="password"
+                                placeholder="Confirm Password"
+                                name='password2'
+                                ref={register({
+                                    //required: true,
+                                    validate: (value) => value === watch('password')
+                                })} />
+                            {errors.password2 && <p className="helper has-text-danger">must match the qrevious entry</p>}
+                        </div>
+                    </div>
+
                     <div className="control">
-                        <input
-                            disabled = {(codeSent)?"":"disabled"}
-                            className={`input ${errors.password && 'is-danger'}`}
-                            type="password"
-                            placeholder="Password"
-                            name='password'
-                            ref={register({
-                                //required: true,
-                                minLength: {
-                                    value: 6,
-                                    message: 'cannot be less than 6 digits'
-                                }
-                            })} />
-                        {errors.password && <p className="helper has-text-danger">{errors.password.message}</p>}
+                        <button 
+                            className="button is-link login_button" 
+                            onClick={handleSubmit(onSubmit2)}
+                            disabled = {(codeSent)?"":"disabled"}>Submit</button>
                     </div>
-                </div>
-
-                <div className="field">
-                    <label className="label">Confirm Password</label>
-                    <div className="control">
-                        <input
-                            disabled = {(codeSent)?"":"disabled"}
-                            className={`input ${errors.password2 && 'is-danger'}`}
-                            type="password"
-                            placeholder="Confirm Password"
-                            name='password2'
-                            ref={register({
-                                //required: true,
-                                validate: (value) => value === watch('password')
-                            })} />
-                        {errors.password2 && <p className="helper has-text-danger">must match the qrevious entry</p>}
-                    </div>
-                </div>
-
-                <div className="control">
-                    <button 
-                        className="button is-link login_button" 
-                        onClick={handleSubmit(onSubmit2)}
-                        disabled = {(codeSent)?"":"disabled"}>Submit</button>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>  
+        </React.Fragment>
     );
 }
