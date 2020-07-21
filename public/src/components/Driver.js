7d95d35e2559a0d09ee4d6ccdb00875c20e42173
Shiyuan Miao <shiyuanm@sfu.ca>
2020-07-21 02:43:10 -0700

    fix bugs

--- public/src/components/Driver.js
+++ public/src/components/Driver.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Map, GoogleApiWrapper } from 'google-maps-react'; 
@@ -61,49 +60,9 @@ class Driver extends Component {
                </div>
                
                <GoogleMaps points={this.state.points} zoom={this.state.zoom} latLng={this.state.latLng}/>
-           </div>
-=======
-// import React, { Component } from 'react';
-// import axios from 'axios';
-// import { Map, GoogleApiWrapper } from 'google-maps-react'; 
-// // import {toast} from 'react-toastify';
-// import GoogleMaps from "./GoogleMaps";
-// import 'css/driver.scss';
+           </div>           
+        )
+    }
+}
 
-// class Driver extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             points: [],
-//             orders: []
-//         }
-//     }
-//     async componentDidMount() {
-//         const res = await axios.post('api/orders/getOrders', {}).then(resp => {
-//             this.setState({
-//                 orders: resp.data
-//             })
-//             console.log(this.state.orders)
-//         })
-//     }
-//     render() {
-//         return (
-//            <div class="driverMain">
-//                <div class="orderList">
-//                    orderList
-//                 {this.state.orders.map((data,idx)=>{
-//                     return(
-//                         <div>{data.phone}</div>
-//                     )
-//                 })}
-//                </div>
-               
-//                <GoogleMaps points={this.state.points}/>
-//            </div>
->>>>>>> Stashed changes
-           
-//         )
-//     }
-// }
-
-// export default Driver;
+export default Driver;
